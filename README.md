c8adf5a52ae00d838b0404200b9bc44c72b0d3c4
YoshiokaKaori <yoshioka9861@gmail.com>
2023-07-27 14:53:34 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular_lesson
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/7ssk4e)
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  template: `
+  <section>
+    <form>
+      <input type="text" placeholder="Filter by city">
+      <button class="primary" type="button">Search</button>
+    </form>
+  </section>
+  <section class="results">
+    <app-housing-location></app-housing-location>
+  </section>
+  `,
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
